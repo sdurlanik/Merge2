209406092f24ed0c1c82f53d2083c562d5079fd4
Samed Durlanık <sametdurlanik@gmail.com>
2025-07-04 18:06:46 +0300

    Feat: Object pooler added

---
+++ Assets/Game/Dev/Scripts/Core/ObjectPooler.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using Sdurlanik.Merge2.Data;
+using UnityEngine;
+
+namespace Sdurlanik.Merge2.Core
+{
+    public class ObjectPooler : MonoBehaviour
+    {
+        public static ObjectPooler Instance { get; private set; }
+
+        private readonly Dictionary<string, Queue<GameObject>> _poolDictionary = new();
+        private readonly Dictionary<string, GameObject> _prefabCache = new();
+        
+        private Transform _pooledObjectsParent;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+
+            _pooledObjectsParent = transform;
+        }
+
+        /// <summary>
+        /// Create a new object pool for a given prefab.
+        /// </summary>
+        /// <param name="prefab">Prefab to pool.</param>
+        /// <param name="initialSize">Initial number of objects to create in the pool.</param>
+        public void CreatePool(PoolSettingsSO poolSettings)
+        {
+            var poolTag = poolSettings.DefaultItemPrefab.name;
+            if (_poolDictionary.ContainsKey(poolTag))
+            {
+                Debug.LogWarning($"Pool with tag '{poolTag}' already exists.");
+                return;
+            }
+
+            var objectPool = new Queue<GameObject>();
+            for (var i = 0; i <  poolSettings.InitialPoolSize; i++)
+            {
+                var obj = Instantiate( poolSettings.DefaultItemPrefab, _pooledObjectsParent);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            _poolDictionary.Add(poolTag, objectPool);
+            _prefabCache.Add(tag,  poolSettings.DefaultItemPrefab);
+        }
+
+        /// <summary>
+        /// Gets an object from the pool by its tag.
+        /// </summary>
+        /// <param name="poolTag">Tag of the prefab to retrieve.</param>
+        public GameObject GetObjectFromPool(string poolTag)
+        {
+            if (!_poolDictionary.ContainsKey(poolTag))
+            {
+                Debug.LogError($"Pool with tag '{poolTag}' doesn't exist.");
+                return null;
+            }
+
+            if (_poolDictionary[poolTag].Count > 0)
+            {
+                var objectToSpawn = _poolDictionary[poolTag].Dequeue();
+                return objectToSpawn;
+            }
+    
+            Debug.LogWarning($"Pool with tag '{poolTag}' is empty. Creating a new instance from cache.");
+    
+            if (_prefabCache.TryGetValue(poolTag, out var prefabToInstantiate))
+            {
+                var newObj = Instantiate(prefabToInstantiate, _pooledObjectsParent);
+                return newObj;
+            }
+    
+            Debug.LogError($"Prefab for tag '{poolTag}' not found in cache. Cannot create new instance.");
+            return null;
+        }
+
+        /// <summary>
+        /// Returns an object to the pool.
+        /// </summary>
+        /// <param name="poolTag"> Tag of the pool to return the object to.</param>
+        /// <param name="objectToReturn"> Object to return to the pool.</param>
+        public void ReturnObjectToPool(string poolTag, GameObject objectToReturn)
+        {
+            if (!_poolDictionary.ContainsKey(poolTag))
+            {
+                Debug.LogWarning($"Trying to return object to a non-existent pool with tag '{poolTag}'. Destroying object instead.");
+                Destroy(objectToReturn);
+                return;
+            }
+            
+            objectToReturn.SetActive(false);
+            objectToReturn.transform.SetParent(_pooledObjectsParent);
+            _poolDictionary[poolTag].Enqueue(objectToReturn);
+        }
+    }
+}
---
+++ Assets/Game/Dev/Scripts/Data/PoolSettingsSO.cs
@@ -0,0 +1,12 @@
+﻿using Sdurlanik.Merge2.Items;
+using UnityEngine;
+
+namespace Sdurlanik.Merge2.Data
+{
+    [CreateAssetMenu(fileName = "PoolSettings", menuName = "Merge2/Settings/PoolSettings", order = 1)]
+    public class PoolSettingsSO : ScriptableObject
+    {
+        public GameObject DefaultItemPrefab;
+        public int InitialPoolSize = 25;
+    }
+}
