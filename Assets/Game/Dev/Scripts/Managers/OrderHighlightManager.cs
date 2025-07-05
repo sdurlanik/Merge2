ebb5f9f3e362ca6b2492e7905efe30f3eb09aefe
Samed Durlanık <sametdurlanik@gmail.com>
2025-07-05 19:12:31 +0300

    Chore: Order highlight added

---
+++ Assets/Game/Dev/Scripts/Managers/OrderHighlightManager.cs
@@ -0,0 +1,67 @@
+﻿// Konum: Sdurlanik.Merge2/Scripts/Managers/OrderHighlightManager.cs (YENİ DOSYA)
+
+using System.Collections.Generic;
+using System.Linq;
+using Sdurlanik.Merge2.Core;
+using Sdurlanik.Merge2.Data;
+using Sdurlanik.Merge2.Data.Orders;
+using Sdurlanik.Merge2.Events;
+using Sdurlanik.Merge2.GridSystem;
+using UnityEngine;
+
+namespace Sdurlanik.Merge2.Managers
+{
+    public class OrderHighlightManager : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            EventBus<BoardStateChangedEvent>.OnEvent += HandleBoardStateChanged;
+            EventBus<ActiveOrdersUpdatedEvent>.OnEvent += HandleOrdersUpdated;
+        }
+
+        private void OnDisable()
+        {
+            EventBus<BoardStateChangedEvent>.OnEvent += HandleBoardStateChanged;
+            EventBus<ActiveOrdersUpdatedEvent>.OnEvent -= HandleOrdersUpdated;
+        }
+
+        private void HandleBoardStateChanged(BoardStateChangedEvent e)
+        {
+            UpdateAllCellHighlights();
+        }
+
+        private void HandleOrdersUpdated(ActiveOrdersUpdatedEvent e)
+        {
+            UpdateAllCellHighlights();
+        }
+
+
+        private void UpdateAllCellHighlights()
+        {
+            var orderManager = ServiceLocator.Get<OrderManager>();
+            var gridManager = ServiceLocator.Get<GridManager>();
+
+            var requiredItemsForCompletableOrders = new HashSet<ItemSO>();
+
+            foreach (var order in orderManager.ActiveOrders)
+            {
+                if (order.Status == OrderStatus.ReadyToComplete)
+                {
+                    foreach (var requirement in order.Requirements)
+                    {
+                        requiredItemsForCompletableOrders.Add(requirement.RequiredItem);
+                    }
+                }
+            }
+
+            var allCells = gridManager.GetAllCells();
+            foreach (var cell in allCells)
+            {
+                var shouldHighlight = !cell.IsEmpty &&
+                                      requiredItemsForCompletableOrders.Contains(cell.OccupiedItem.ItemDataSO);
+
+                cell.SetHighlight(shouldHighlight);
+            }
+        }
+    }
+}
