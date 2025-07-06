c4c5e960eb23c9aebe9e9c8eeb08e1d27bcdbded
Samed Durlanık <sametdurlanik@gmail.com>
2025-07-07 01:36:04 +0300

    Feat: Camera resizer added

---
+++ Assets/Game/Dev/Scripts/Core/CameraResizer.cs
@@ -0,0 +1,30 @@
+﻿using Sdurlanik.Merge2.Data;
+using UnityEngine;
+
+namespace Sdurlanik.Merge2.Core
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraResizer : MonoBehaviour
+    {
+        [SerializeField] private CameraSettingsSO _cameraSettings;
+
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+            AdjustCameraSize();
+        }
+
+        private void AdjustCameraSize()
+        {
+            var referenceAspect = _cameraSettings.ReferenceWidth / _cameraSettings.ReferenceHeight;
+            
+            var currentAspect = (float)Screen.width / Screen.height;
+            
+            var newOrthographicSize = _cameraSettings.ReferenceOrthographicSize * (referenceAspect / currentAspect);
+            
+            _camera.orthographicSize = newOrthographicSize;
+        }
+    }
+}
---
+++ Assets/Game/Dev/Scripts/Data/CameraSettingsSO.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Sdurlanik.Merge2.Data
+{
+    [CreateAssetMenu(fileName = "CameraSettings", menuName = "Merge2/Settings/Camera Settings")]
+    public class CameraSettingsSO : ScriptableObject
+    {
+        public float ReferenceWidth = 1080f;
+        
+        public float ReferenceHeight = 1920f;
+        
+        public float ReferenceOrthographicSize = 14f;
+    }
+}
